80a236a803f2e2cb8f28bd8170146fbb3d9ed0b2
Roman Piller <roman.piller@gmail.com>
2026-06-21 10:56:30 +0200

    mazanie gitignore v temp v man teste

--- tests/manual/bootstrap.php
+++ tests/manual/bootstrap.php
@@ -5,13 +5,11 @@ require __DIR__ . '/../../vendor/autoload.php';
 use Nette\DI\ContainerLoader;
 use Nette\DI\Extensions\ExtensionsExtension;
 use Tester\Environment;
-use Tester\Helpers;
 
 Environment::setup();
 Environment::bypassFinals();
 date_default_timezone_set('Europe/Bratislava');
 
-Helpers::purge(__DIR__ . '/temp');
 $containerLoader = new ContainerLoader(__DIR__ . '/temp', true);
 
 $container = $containerLoader->load(function ($compiler) {
